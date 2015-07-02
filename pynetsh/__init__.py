f8254c1c4d17772fc0772261ee3c1f0800c8eeba
reedcourty <reedcourty@gmail.com>
2015-07-02 19:55:30 +0200

    Added Network class, implemented some function

--- pynetsh/__init__.py
+++ pynetsh/__init__.py
@@ -4,11 +4,21 @@
 import subprocess
 import sys
 
+class Network:
+    def __init__(self, name, bssid_number=None):
+        self.name = name
+        self.bssid_number = bssid_number
+
+    def __repr__(self):
+        return self.name
+
 class NetshWLAN:
     def __init__(self):
-        pass
+        self.networks = []
 
-    def get_networks(self, interface=None, mode=None):
+    def get_networks(self, interface=None, mode=None, show=False):
+        if (mode not in ["bssid", "ssid", None]):
+            raise Exception("Wrong mode parameter is specified.")
         cmd = "netsh wlan show networks"
         if interface is not None:
             cmd = "{} interface={}".format(cmd, interface)
@@ -16,18 +26,43 @@ class NetshWLAN:
             cmd = "{} mode={}".format(cmd, mode)
         p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         out_raw, err_raw = p.communicate()
+
         out_decoded = out_raw.decode(sys.stdout.encoding)
         out = out_decoded.split('\r\n')
-        return out
+        if (len(out)==2):
+            raise Exception("The command ({}) failed :(".format(cmd))
+
+        number_of_networks = [int(s) for s in out[2].split() if s.isdigit()][0]
+
+        list_of_raw_networks = []
+        raw_network = []
+        for line in out[4:]:
+            if (line == ''):
+                list_of_raw_networks.append(raw_network)
+                raw_network = []
+            else:
+                raw_network.append(line)
+
+        list_of_raw_networks = list_of_raw_networks[0:-1]
+        
+        #print(number_of_networks)
+        
+
+        for i in list_of_raw_networks:
+            self.networks.append(Network(i[0].split(" : ")[1]))
+
+
+
+
+        if show:
+            return out_raw
+
+
+        
+        
+        #for i in xrange(4, len(out), )
+
+        return self.networks
 
     def show_networks(self, interface=None, mode=None):
-        cmd = "netsh wlan show networks"
-        if interface is not None:
-            cmd = "{} interface={}".format(cmd, interface)
-        if mode is not None:
-            cmd = "{} mode={}".format(cmd, mode)
-        p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        out_raw, err_raw = p.communicate()
-        out_decoded = out_raw.decode(sys.stdout.encoding)
-        out = out_decoded.split('\r\n')
-        print(out_decoded)
+        print(self.get_networks(interface, mode, show=True))
--- test_pynetsh.py
+++ test_pynetsh.py
@@ -7,14 +7,29 @@ import pynetsh
 
 class NetshWLANTestCase(unittest.TestCase):
 
+    @unittest.skip(None)
     def test_show_networks(self):
         # Arrange
         netshwlan = pynetsh.NetshWLAN()
-        
+        netshwlan.show_networks(mode="bssid")
+
         # Act
         
         expected = True
-        actual = netshwlan.show_networks(mode="gssid")
+        actual = True
+        
+        # Assert
+        self.assertEqual(expected, actual, msg='Expected "{}", but get "{}"'.format(expected, actual))
+
+    #@unittest.skip(None)
+    def test_get_networks(self):
+        # Arrange
+        netshwlan = pynetsh.NetshWLAN()
+        print(netshwlan.get_networks(mode="ssid"))
+        # Act
+        
+        expected = True
+        actual = True
         
         # Assert
         self.assertEqual(expected, actual, msg='Expected "{}", but get "{}"'.format(expected, actual))
